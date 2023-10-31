4de03ab1bee6145a03d9e7223d8b49395ed228b6
unknown <mriuse@fp.insjoaquimmir.cat>
2023-10-31 17:07:56 +0100

    Fix part 6

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   Log::info('Loading welcome page');
+   return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function (Request $request) {
+   $request->session()->flash('info', 'TEST flash messages');
+   return view('dashboard');
+})->middleware(['auth','verified'])->name('dashboard');;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
