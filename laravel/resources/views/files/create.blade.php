1ece1255e4939c2982df757b0f5b55c1e2bb6d43
mriuse <mriuse@fp.insjoaquimmir.cat>
2023-11-07 17:09:13 +0100

    New TailwindCSS layouts

--- laravel/resources/views/files/create.blade.php
+++ laravel/resources/views/files/create.blade.php
@@ -1,11 +1,24 @@
 @include('partials.flash')
 
-<form method="post" action="{{ route('files.store') }}" enctype="multipart/form-data">
-   @csrf
-   <div class="form-group">
-       <label for="upload">File:</label>
-       <input type="file" class="form-control" name="upload"/>
-   </div>
-   <button type="submit" class="btn btn-primary">Create</button>
-   <button type="reset" class="btn btn-secondary">Reset</button>
-</form>
+<x-app-layout>
+   <x-slot name="header">
+       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+           {{ __('Files') }}
+       </h2>
+   </x-slot>
+   <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
+                <h1 class="text-2xl font-semibold mb-4">Upload file</h1>
+                <form method="post" action="{{ route('files.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" class="border p-2 w-full mb-4" name="upload"/>
+                    <div class="flex justify-between">
+                        <button type="reset" class="w-1/4 bg-gray-400 text-white py-2 px-10 rounded hover:bg-gray-500 active:outline-none active:ring active:ring-gray-300">Reset</button>
+                        <button type="submit" class="w-1/4 bg-emerald-500 text-white py-2 px-10 rounded hover:bg-emerald-600 active:outline-none active:ring active:ring-emerald-300">Upload</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- laravel/resources/views/files/index.blade.php
+++ laravel/resources/views/files/index.blade.php
@@ -8,34 +8,41 @@
    </x-slot>
 
 
-   <div class="py-12">
-       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-               <div class="p-6 bg-white border-b border-gray-200">
-                   <table class="table">
-                      <thead>
-                          <tr>
-                              <td scope="col">ID</td>
-                              <td scope="col">Filepath</td>
-                              <td scope="col">Filesize</td>
-                              <td scope="col">Created</td>
-                              <td scope="col">Updated</td>
-                          </tr>
-                      </thead>
-                      <tbody>
-                          @foreach ($files as $file)
-                          <tr>
-                              <td>{{ $file->id }}</td>
-                              <td>{{ $file->filepath }}</td>
-                              <td>{{ $file->filesize }}</td>
-                              <td>{{ $file->created_at }}</td>
-                              <td>{{ $file->updated_at }}</td>
-                          </tr>
-                          @endforeach
-                      </tbody>
-                  </table>
-               </div>
-           </div>
-       </div>
-   </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10">
+                <h1 class="text-2xl font-semibold mb-4">File list</h1>
+                <table class="w-full table-auto">
+                    <thead>
+                        <tr>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">ID</td>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">Filepath</td>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">Filesize</td>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">Created</td>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">Updated</td>
+                            <th class="bg-gray-200 text-gray-700 py-2 px-4 text-center">Actions</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($files as $file)
+                        <tr>
+                            <td class="py-2 px-4 text-center">{{ $file->id }}</td>
+                            <td class="py-2 px-4 text-center">{{ $file->filepath }}</td>
+                            <td class="py-2 px-4 text-center">{{ $file->filesize }}</td>
+                            <td class="py-2 px-4 text-center">{{ $file->created_at }}</td>
+                            <td class="py-2 px-4 text-center">{{ $file->updated_at }}</td>
+                            <td class="py-2 px-4 text-center">
+                                <div class="flex flex-col w-full space-y-1">
+                                    <button type="view" class="w-full bg-gray-400 text-white py-2 px-10 rounded hover:bg-gray-500 active:outline-none active:ring active:ring-gray-300">View more</button>
+                                    <button type="edit" class="w-full bg-gray-400 text-white py-2 px-10 rounded hover:bg-gray-500 active:outline-none active:ring active:ring-gray-300">Edit</button>
+                                    <button type="delete" class="w-full bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 active:outline-none active:ring active:ring-red-300">Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
