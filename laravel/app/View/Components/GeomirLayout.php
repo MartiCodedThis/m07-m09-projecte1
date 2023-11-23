320a74195c399ad4dd75192bfb8296f3fa4e27b3
mriuse <mriuse@fp.insjoaquimmir.cat>
2023-11-23 19:22:15 +0100

    Fix layoutgit status!

--- laravel/app/View/Components/GeomirLayout.php
+++ laravel/app/View/Components/GeomirLayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class GeoMirLayout extends Component
+class GeomirLayout extends Component
 {
     /**
      * Create a new component instance.
