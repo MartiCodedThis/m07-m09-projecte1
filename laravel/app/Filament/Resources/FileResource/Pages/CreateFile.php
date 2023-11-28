85b324fd1429e4452cc178323280bb4f51b1ab14
Martí <msolert@fp.insjoaquimmir.cat>
2023-11-28 17:50:51 +0100

    WIP 2.6.2 Relació File - Places

--- laravel/app/Filament/Resources/FileResource/Pages/CreateFile.php
+++ laravel/app/Filament/Resources/FileResource/Pages/CreateFile.php
@@ -5,8 +5,6 @@ namespace App\Filament\Resources\FileResource\Pages;
 use App\Filament\Resources\FileResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Support\Facades\Storage;
-
 
 class CreateFile extends CreateRecord
 {
